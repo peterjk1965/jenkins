7460c04f390ce5b7c00f4e20dd7ba84a1aabcc4a
Peter <peter.j.k@hotmail.com>
2019-07-14 13:05:21 -0500

    First commit for all my jenkins files

---
+++ install.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+sudo apt-get update
+sudo apt-get upgrade -y
+sudo apt install openjdk-8-jdk -y
+wget -q -O - https://pkg.jenkins.io/debian/jenkins.io.key | sudo apt-key add -
+sudo sh -c 'echo deb http://pkg.jenkins.io/debian-stable binary/ > /etc/apt/sources.list.d/jenkins.list'
+sudo apt update
+sudo apt install jenkins
+service jenkins status
+sudo cat /var/lib/jenkins/secrets/initialAdminPassword
+
+exit 0
